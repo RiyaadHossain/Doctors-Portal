2f3e2f62f3f44e201e203694413149d534052cb7
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-15 18:02:48 +0600

    athorize someonde as admin - secured

--- src/Components/Pages/DashBoard/UsersRow.jsx
+++ src/Components/Pages/DashBoard/UsersRow.jsx
@@ -9,24 +9,34 @@ const UsersRow = ({ user, i, refetch }) => {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if(res.status === 401){
+          toast.error("You aren't an Admin")
+        }
+        return res.json()
+      })
       .then((data) => {
-        toast.success('User is Admin Now')
-        refetch()
-        console.log(data)});
-     
+        if (data.modifiedCount) {
+          toast.success("User is Admin Now");
+          refetch();
+        }
+      });
   };
   return (
     <tr>
       <th className="text-gray-700">{i + 1}</th>
       <td className="text-gray-700">{user.email}</td>
       <td className="text-gray-700">
-        {user.role !== 'admin' ? <button
-          onClick={makeAdmin}
-          className="btn text-gray-700 hover:text-gray-50 border-gray-700 btn-xs"
-        >
-          Make Admin
-        </button> : <p className="text-secondary">Already Admin</p>}
+        {user.role !== "admin" ? (
+          <button
+            onClick={makeAdmin}
+            className="btn text-gray-700 hover:text-gray-50 border-gray-700 btn-xs"
+          >
+            Make Admin
+          </button>
+        ) : (
+          <p className="text-secondary">Already Admin</p>
+        )}
       </td>
       <td className="text-gray-700">
         <button className="btn text-gray-700 hover:text-gray-50 border-gray-700 btn-xs">
